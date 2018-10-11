e52b5fdb3ba58e039e865b6ab669e54a1c2794c6
vegajavier <vegajavier86@gmail.com>
2018-10-10 23:05:02 -0500

    liri with some omdb code not working yet

--- liri.js
+++ liri.js
@@ -1,21 +1,32 @@
 require("dotenv").config();
+
 let keys = require("./keys.js");
+let  fs = require('fs');
 let Spotify = require('node-spotify-api');
+let spotify = new Spotify(keys.spotify);
 let request = require("request");
+let liri = process.argv[2];
+let name = process.argv[3];
+console.log(keys);
 
-request("http://www.omdbapi.com/?i=tt3896198&apikey=79176496", function(error,response,body){
+if (liri === 'spotify-this'){
+    spotifyThis();
+} else if(liri === 'movie-this'){
+    movieThis();
+}else if(liri === 'concert-this'){
+    concertThis();
+} else if(liri === 'do-what-it-says'){
+    doWhatItSays();
+};
 
-})
+function movieThis() {
+	let queryURL = "http://www.omdbapi.com/?i=" + name + "tt3896198&apikey=79176496";
+		request (queryURL, function(err, response, body){
+			let movieData = JSON.parse(body);
+			if(!err && response.statusCode === 200){
+				let movieDisplay =
+				" ______________________" + "\n"
 
-
-function spotifyMe(){
-	console.log("Music for DAYS!");
+			}
+		})
 }
-
-function spotifyMe(music)
-	this.concert = true;
-	this.spotifyThisSong = true;
-	this.movieThis = true;
-	this.doWhatItSays = true;
-	
-	let spotify = new Spotify(keys.spotify);
