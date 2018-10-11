81b9128259b78bf04de4f0f447f53e1c189eeea1
vegajavier <vegajavier86@gmail.com>
2018-10-10 19:47:27 -0500

    trying to understand

--- liri.js
+++ liri.js
@@ -1,3 +1,16 @@
+require("dotenv").config();
 let keys = require("./keys.js");
-
 let Spotify = require('node-spotify-api');
+
+
+function spotifyMe(){
+	console.log("Music for DAYS!");
+}
+
+function spotifyMe(music)
+	this.concert = true;
+	this.spotifyThisSong = true;
+	this.movieThis = true;
+	this.doWhatItSays = true;
+	
+	let spotify = new Spotify(keys.spotify);
